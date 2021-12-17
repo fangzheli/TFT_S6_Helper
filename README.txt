d0e42cabd1bea7ba4ec8eb30a4eacadfbab5d6cd
fangzheli <48412604+fangzheli@users.noreply.github.com>
2021-12-17 01:10:46 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,26 @@
+SI507 Project Checkpoint - League of Legends TFT Helper for Season 6
+Teamfight Tactics (TFT) is an auto battler game based on the universe of League of Legends (LOL). You are supposed to choose champions from a store to fight against other 7 players. Synergy system is the most essential system in an auto battler game. Each champion in TFT has at least one, at most three traits with them. With the synergy, different drafts can have extra abilities. However, there are 28 types of traits there, so it is very hard for rookie like me to remember. An idea to help with this situation is that people put in the champions they have for now and a helper program gives some suggestions on the potential draft I can build based on both the win rate and my current draft. To simplify the question, we will not take the action of other players into consideration because in most case, the action of other players will only affect the level of your champions instead of the draft of your team. Also, hextech augments are not considered because most of them only affect the intensity of your team instead of the draft of your team. Here are some sample screenshots for the data source.
+
+Data Source
+All the data I need is the whole list of champions and their traits, the whole list of traits and their effects and the win rate for different drafts. The whole list of champions and their traits can be got by scraping https://tftactics.gg/db/champions. The win rate for different drafts can be scraped from https://tftactics.gg/meta-report. I check the robots.txt file of this website and I am allowed to scrap anything.
+    
+Data Access and Storage
+I use selenium to get the page I want because this website use JavaScript to load the pages, so I use Selenium and pause for a few second and get the html file. Then I use beautifulsoup to parse it and it works well.
+ 
+Data Processing
+A binary search tree for different traits with their win rates. After the user inputs the champions that they have currently, the helper will check the traits and search it in the binary search tree to get its win rate.
+
+Data Presentation
+A Flask app is created to demonstrate the content.
+
+How to use it
+The default homepage for TFT helper is http://127.0.0.1:5000/helper
+Two functions get_champions() and get_traits_rank() are used to get the dynamic websites by selenium. Two functions clean_champions_synergy() and clean_traits_rank() are used to process the two htmls mentioned above to json. These four functions only need to be runned when there is a need to update or rescrap the page. When running the flask, there is no need for running these functions so I have commented out these functions in the main functions.
+
+The known problem
+1. When people don't click any champions but clicking "get recommendation", they may get a very simple page to tell them there is not enough champions clicked.
+2. When too many champions are chosen, it will take a very long time to go through every combination of comps.
+
+Future Work
+1. improve the recommendation algorithm
+2. add and beautify flask page
--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 import json
-
+import collections
+import itertools
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+import os
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/helper')
 def helper():
-    champions =
-    return '<h1>Hello World!</h1>'
+    return render_template('input.html', champions_=champions_, champions=champions)
+
+
+@app.route('/recommendation', methods=['POST'])
+def give_recommendation():
+    owned_champions_list = []
+    level = int(request.form["levels"])
+    # stage = request.form["Stage"]
+    for champion in request.form.to_dict().keys():
+        for _key, _value in champions_.items():
+            if _value == champion:
+                owned_champions_list.append(_key)
+                break
+    if not owned_champions_list:
+        return "<h1> Oops! It seems that not enough champions are chosen!</h1>"
+    comps = list(itertools.combinations(owned_champions_list, min(level, len(owned_champions_list))))
+    win_rate_list = []
+    for comp in comps:
+        trait = check_trait(comp, champions_synergy_origin, champions_synergy_class)
+        data_node = search(root, trait)
+        if data_node:
+            win_rate_list.append(float(data_node.win_rate.strip('%'))/100)
+        else:
+            win_rate_list.append(0)
+    win_rate = max(win_rate_list)
+    recommendation_comp = comps[win_rate_list.index(max(win_rate_list))]
+    return render_template('recommendation.html', champions=owned_champions_list, win_rate=win_rate, recommendation_comp=recommendation_comp)
 
 
 def get_champions():
@@ -27,6 +56,7 @@ def get_champions():
 
 
 def clean_champions_synergy():
+    dirname = os.path.dirname(__file__)
     f = open("champions.html", "r", encoding="utf-8")
     html = f.read()
     soup = BeautifulSoup(html, 'html.parser')
@@ -37,6 +67,11 @@ def clean_champions_synergy():
     for champion in champions_names:
         try:
             champions.append(champion.contents[1])
+            response = requests.get(champion.contents[0].contents[0]['src'])
+            file_name = dirname + "/static/imgs/" + champion.contents[1] + ".png"
+            file = open(file_name, "wb")
+            file.write(response.content)
+            file.close()
         except IndexError:
             pass
     divs = soup.find_all("div", class_='rt-tr-group')
@@ -61,7 +96,7 @@ def clean_champions_synergy():
             champions_cost[name] = cost
         except IndexError:
             pass
-    results = {"champions": champions, "champions_synergy_orgin": champions_synergy_origin,
+    results = {"champions": champions, "champions_synergy_origin": champions_synergy_origin,
                "champions_synergy_class": champions_synergy_class, "champions_cost": champions_cost}
     champions_json = json.dumps(results)
     f = open("champions.json", "w", encoding="utf-8")
@@ -100,7 +135,7 @@ def clean_traits_rank():
             trait_data.append(win_rate)
             trait_data.append(top4_rate)
             trait_data.append(popularity)
-            traits_rank[trait_name+active_number] = trait_data
+            traits_rank[trait_name + active_number] = trait_data
         except IndexError:
             pass
     traits_rank_json = json.dumps(traits_rank)
@@ -149,8 +184,27 @@ def inorder(root):
         inorder(root.right)
 
 
+def check_trait(champions_list, champions_origin, champions_class):
+    '''
+    :param champions_class:
+    :param champions_origin:
+    :param champions_list:
+    :return: a string that combine the strongest trait and the number of champions that activate this trait
+    '''
+    synergy = []
+    for champion in champions_list:
+        for trait in champions_class[champion]:
+            synergy.append(trait)
+        for trait in champions_origin[champion]:
+            synergy.append(trait)
+    cnt = collections.Counter(synergy)
+    strong_trait = cnt.most_common(1)[0][0]
+    strong_trait_number = cnt.most_common(1)[0][1]
+    return strong_trait+str(strong_trait_number)
+
+
 if __name__ == '__main__':
-    # data collection
+    # data collection, only use when you need to update the cache
     # get_traits_rank()
     # get_champions()
     # clean_champions_synergy()
@@ -165,7 +219,7 @@ if __name__ == '__main__':
     champions_dict = json.loads(champions_json)
     f.close()
     champions = champions_dict["champions"]
-    champions_synergy_origin = champions_dict["champions_synergy_orgin"]
+    champions_synergy_origin = champions_dict["champions_synergy_origin"]
     champions_synergy_class = champions_dict["champions_synergy_class"]
     champions_cost = champions_dict["champions_cost"]
     # make the assumption mentioned in the proposal
@@ -178,14 +232,14 @@ if __name__ == '__main__':
     del traits_rank['Glutton1']
     del traits_rank['Transformer1']
     # start to build tree based on current trait
-    root = TraitRankNode('Assassin8', traits_rank['Assassin8'][0], traits_rank['Assassin8'][1], traits_rank['Assassin8'][2], traits_rank['Assassin8'][3])
+    root = TraitRankNode('Assassin8', traits_rank['Assassin8'][0], traits_rank['Assassin8'][1],
+                         traits_rank['Assassin8'][2], traits_rank['Assassin8'][3])
     del traits_rank['Assassin8']
     for key, value in traits_rank.items():
         root = insert(root, key, value[0], value[1], value[2], value[3])
     # inorder(root)
-    print("Hi, TFT")
+    # create the champions name without space
+    champions_ = {champion: champion.replace(" ", "_") for champion in champions}
     # run flask
-    print('starting Flask app', app.name)
+    print("Hi, TFT helper")
     app.run(debug=True)
-
-
